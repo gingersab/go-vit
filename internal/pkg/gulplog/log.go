0f04b1ab2eb67dec8e989ed965f8974f1f3ab85b
Scott <scott.wares@yahoo.co.uk>
2025-01-30 20:52:53 +0000

    Adding additional custom logging formatting

--- internal/pkg/gulplog/log.go
+++ internal/pkg/gulplog/log.go
@@ -7,5 +7,7 @@ import (
 
 var Info = log.New(os.Stdout, "\u001b[34mINFO: \u001B[0m", log.LstdFlags|log.Lshortfile)
 var Warning = log.New(os.Stdout, "\u001b[33mWARNING: \u001B[0m", log.LstdFlags|log.Lshortfile)
-var Error = log.New(os.Stdout, "\u001b[31mERROR: \u001b[0m", log.LstdFlags|log.Lshortfile)
+var Error = log.New(os.Stdout, "\u001b[32mERROR: \u001b[0m", log.LstdFlags|log.Lshortfile)
 var Debug = log.New(os.Stdout, "\u001b[36mDEBUG: \u001B[0m", log.LstdFlags|log.Lshortfile)
+var Fatal = log.New(os.Stdout, "\u001b[31mDEBUG: \u001B[0m", log.LstdFlags|log.Lshortfile)
+var Panic = log.New(os.Stdout, "\u001b[30mDEBUG: \u001B[0m", log.LstdFlags|log.Lshortfile)
